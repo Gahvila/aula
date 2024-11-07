a76dde256c3c8be318ea1bf9cd1f665f31a43142
DaJokni <56400974+DaJokni@users.noreply.github.com>
2024-11-08 01:30:44 +0200

    päivitetty 1.21.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,17 +22,17 @@ dependencies {
     compileOnly("de.hexaoxi:carbonchat-api:3.0.0-beta.27")
     compileOnly ("com.github.koca2000:NoteBlockAPI:1.6.2")
     implementation("com.github.simplix-softworks:simplixstorage:3.2.7")
-    implementation ("com.github.stefvanschie.inventoryframework:IF:0.10.17")
-    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+    implementation ("com.github.stefvanschie.inventoryframework:IF:0.10.18")
+    compileOnly("io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT")
 
     //commandapi
-    compileOnly("dev.jorel:commandapi-annotations:9.5.3")
-    implementation("dev.jorel:commandapi-bukkit-shade:9.5.3")
-    annotationProcessor("dev.jorel:commandapi-annotations:9.5.3")
+    compileOnly("dev.jorel:commandapi-annotations:9.6.1")
+    implementation("dev.jorel:commandapi-bukkit-shade-mojang-mapped:9.6.1")
+    annotationProcessor("dev.jorel:commandapi-annotations:9.6.1")
 }
 
 group = "Aula"
-version = "3.0"
+version = "3.1"
 description = "Aula"
 java.sourceCompatibility = JavaVersion.VERSION_21
 
@@ -43,6 +43,14 @@ publishing {
 }
 
 tasks {
+    processResources {
+        val props = mapOf("version" to project.version)
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("paper-plugin.yml") {
+            expand(props)
+        }
+    }
     compileJava {
         options.release = 21
     }
@@ -54,11 +62,6 @@ tasks {
     }
     shadowJar {
         archiveFileName.set("${rootProject.name}-${version}.jar")
-        dependencies {
-            include(dependency("dev.jorel:commandapi-bukkit-shade:9.5.3"))
-            include(dependency("com.github.simplix-softworks:simplixstorage:3.2.7"))
-            include(dependency("com.github.stefvanschie.inventoryframework:IF:0.10.17"))
-        }
         relocate("dev.jorel.commandapi", "net.gahvila.aula.shaded.commandapi")
         relocate("de.leonhard.storage", "net.gahvila.aula.shaded.storage")
         relocate ("com.github.stefvanschie.inventoryframework", "net.gahvila.aula.shaded.inventoryframework")
